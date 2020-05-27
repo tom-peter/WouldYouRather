cab391a30dd906cd461bfefcdb34844270491a9a
Tamas Peter <me@example.com>
2020-05-27 17:27:13 +0200

    feat: The user can alternate between viewing answered and unanswered polls
     - refactor Home component
     - the user can alternate between viewing answered and unanswered polls
     - the unanswered polls are shown by default

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,43 +4,52 @@ import { connect } from 'react-redux';
 import Poll from './Poll';
 
 class Home extends React.Component {
+  state = {
+    showUnanswered: true
+  }
+
+  changeList(e, showUnanswered) {
+    e.target.id === 'unanswered' && showUnanswered === false &&
+      this.setState({ showUnanswered: true });
+    e.target.id === 'answered' && showUnanswered === true &&
+      this.setState({ showUnanswered: false });
+  }
+
+  getAnswer(poll, answered) {
+    return (answered ? this.props.users[this.props.authedUser].answers[poll.id] : null );
+  }
+
+  listSelectedPolls(selectedPolls, answered) {
+    return (
+      <ul>
+        { selectedPolls.map((poll) => (
+          <li key={poll.id}>
+            <Poll 
+              poll={poll} 
+              name={this.props.users[poll.author].name} 
+              avatar={this.props.users[poll.author].avatarURL} 
+              answer={this.getAnswer(poll, answered)}
+            />
+          </li>
+        )) }
+      </ul>
+    )
+  }
 
   render() {
     return (
       <div>
         <h3>Home</h3>
-          <div>
-            <h4>Unanswered</h4>
-            <ul>
-              { this.props.unansweredPolls.map((poll) => (
-                <li key={poll.id}>
-                  <Poll 
-                    poll={poll} 
-                    name={this.props.users[poll.author].name} 
-                    avatar={this.props.users[poll.author].avatarURL} 
-                    answer={null}
-                    type='unanswered' 
-                  />
-                </li>
-              )) }
-            </ul>
-          </div>
-          <div>
-            <h4>Answered</h4>
-            <ul>
-              { this.props.answeredPolls.map((poll) => (
-              <li key={poll.id}>
-                <Poll 
-                  poll={poll} 
-                  name={this.props.users[poll.author].name} 
-                  avatar={this.props.users[poll.author].avatarURL} 
-                  answer={this.props.users[this.props.authedUser].answers[poll.id]}
-                  type='answered' 
-                />
-              </li>
-              )) }
-            </ul>          
-          </div>
+        <div id='unanswered' onClick={ (e) => this.changeList(e, this.state.showUnanswered) }>
+          Unanswered
+        </div>
+        <div id='answered' onClick={ (e) => this.changeList(e, this.state.showUnanswered) }>
+          Answered
+        </div>            
+        { this.state.showUnanswered ? 
+          this.listSelectedPolls(this.props.unansweredPolls, false) :
+          this.listSelectedPolls(this.props.answeredPolls, true)
+        }
       </div>
     );
   }
