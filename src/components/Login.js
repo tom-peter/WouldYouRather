c0a230e34fb5489f1121df65306afe2551d979cf
Tamas Peter <me@example.com>
2020-05-28 23:26:29 +0200

    feat: Create login functionality
     - create login functionality in Login component
     - add styling to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,30 +6,70 @@ import { handleInitialUserData } from '../actions/users'
 import { handleInitialQuestionData } from '../actions/questions'
 
 class Login extends React.Component {
+  state = {
+    value: null
+  }
+
+  handleChange = e => {
+    let value = e.target.value;
+    if (value === '0') value = null;
+    this.setState(() => ({
+      value
+    }));
+  }
 
   handleSubmit = e => {
     e.preventDefault();
-    let id = 'robinson';
+    // let id = 'robinson';
+    let id = this.state.value;
     this.props.dispatch(setAuthedUser(id))
     // Get initial question data
     this.props.dispatch(handleInitialQuestionData());
-  };
+  }
   
   componentDidMount() {
 	  // Get initial user data
     this.props.dispatch(handleInitialUserData());
   }
 
+  // Select user form
+  selectUser() {
+    const users = this.props.users;
+    const IDs = Object.keys(users);
+    console.log(users, IDs);
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>Please select your username, and log in.</div>
+        <div className="login-select">
+          <select onChange={ this.handleChange }>
+            <option value="0" key="0"> - - Please select your username - - </option>
+            { IDs.map((id) =>(
+              <option value={ id } key={ id }>
+                { users[id].name }
+              </option>
+            )) }
+          </select>
+        </div>
+        <button onClick={this.handleSubmit} className="btn" type="button" disabled={ this.state.value === null }>
+          Login
+        </button>
+      </form>
+    )
+  }
+  
   render() {
 
     console.log('Login render props', this.props)
+    const IDs = Object.keys(this.props.users); 
 
     return (      
-      <div>
-        <h3>Login</h3>
-        <button onClick={this.handleSubmit} className="btn" type="button">
-          Login
-        </button>
+      <div className="main">
+        <h1>Would you rather ...</h1>
+        <div className="login">
+          <h2>Login</h2>
+          { IDs.length !== 0 && this.selectUser() }
+        </div>
       </div>
     );
   }
